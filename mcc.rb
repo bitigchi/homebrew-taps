b43a7e238159350a6b0f9f62d409f105613c1d9d
Emir Sarı <bitigchi@me.com>
2020-03-08 23:34:44 +0300

    Bump version

--- mcc.rb
+++ mcc.rb
@@ -1,15 +1,15 @@
 class Mcc < Formula
   desc "Basic text-to-morse converter for the command-line, written in Swift"
   homepage "https://github.com/bitigchi/mcc"
-  version "0.1.5"
+  version "0.1.6"
   url "https://github.com/bitigchi/mcc/archive/v0.1.5.tar.gz"
-  sha256 "73ee87b02a4d8d7096bcf9b6ea5d3594e82c73e3c3231d355be06952e71e2c93"
+  sha256 "0438295797b07ac2aa45cbe622db0fb57c00b9ba3af163575de4dcbca5f2938d"
   head "https://github.com/bitigchi/mcc.git"
 
   depends_on :xcode => ["11.0", :build]
 
   def install
-    system "swift", "build", "--disable-sandbox"
+    system "make", "install"
   end
 
   test do
