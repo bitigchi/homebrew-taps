e7e0fd81d642d8a2807a3c2c8dc636c2c2b29b66
Emir Sarı <bitigchi@me.com>
2020-03-08 22:20:32 +0300

    Add mcc formula

---
+++ mcc.rb
@@ -0,0 +1,18 @@
+class Mcc < Formula
+  desc "Basic text-to-morse converter for the command-line, written in Swift"
+  homepage "https://github.com/bitigchi/mcc"
+  version "0.1.5"
+  url "https://github.com/bitigchi/mcc/archive/v0.1.5.tar.gz"
+  sha256 "73ee87b02a4d8d7096bcf9b6ea5d3594e82c73e3c3231d355be06952e71e2c93"
+  head "https://github.com/bitigchi/mcc.git"
+
+  depends_on :xcode => ["11.0", :build]
+
+  def install
+    system "make install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    system "false"
+  end
+end
