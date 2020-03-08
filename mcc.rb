91769f0c65bf7b14ed7221bd3db61becf3c82972
Emir Sarı <bitigchi@me.com>
2020-03-08 23:36:03 +0300

    Bump version

--- mcc.rb
+++ mcc.rb
@@ -2,7 +2,7 @@ class Mcc < Formula
   desc "Basic text-to-morse converter for the command-line, written in Swift"
   homepage "https://github.com/bitigchi/mcc"
   version "0.1.6"
-  url "https://github.com/bitigchi/mcc/archive/v0.1.5.tar.gz"
+  url "https://github.com/bitigchi/mcc/archive/0.1.6.tar.gz"
   sha256 "0438295797b07ac2aa45cbe622db0fb57c00b9ba3af163575de4dcbca5f2938d"
   head "https://github.com/bitigchi/mcc.git"
 
